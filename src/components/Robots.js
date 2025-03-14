d7e371d5732a8dd4dff17d38808049340442bdee
lauram354 <l.murciac@uniandes.edu.co>
2025-03-14 09:05:16 -0500

    robots

--- src/components/Robots.js
+++ src/components/Robots.js
@@ -1,8 +1,75 @@
 
+import Table  from 'react-bootstrap/Table';
+import Detail from './Detail';
+import Col from 'react-bootstrap/esm/Col';
+
+
+const { useEffect, useState } = require("react");
+
 function Robots(){
+    const [robots, setRobots] = useState([]);
+    const [detail, setDetail] = useState();
+
+    useEffect(()=>{
+        const URL = "http://localhost:3001/robots";
+        fetch(URL)
+            .then(data => data.json())
+            .then(data => {
+                setRobots(data);
+            });
+    }, []);
+
+    const renderDetail = () =>{
+        if (detail){
+            return(
+                <div>
+            
+                <div className="card" style={{width: "18rem"}}>
+                <img className="card-img-top" src="" alt=""/>
+                <div className="card-body">
+                    <h5 className="card-text">
+                        "hola soy robot"
+                    </h5>
+                    <h6 className="card-text">"h"</h6>
+                </div>
+                </div>
+
+                </div>
+            )
+        }
+    }
+
+    const handleDetail = (robot) =>{
+        setDetail(robot)
+    }
+
     return(
         <div>
-            <h1> Robots</h1>
+            <h1> Adopta tu robot con Robot Lovers!</h1>
+            <div>
+                <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Modelo</th>
+                                <th>Empresa Fabricante</th>
+                            </tr>
+                        </thead>
+                        <tbody >
+                            {robots.map((robot) => (
+                                <tr key={robot.id} onClick= {() => {handleDetail(robot)}}>
+                                    <td>{robot.id} </td>
+                                    <td>{robot.nombre}</td>
+                                    <td>{robot.modelo }</td>
+                                    <td>{robot.empresaFabricante}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+
+            </div>
+
         </div>
     )
 };
